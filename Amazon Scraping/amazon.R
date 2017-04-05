2cd746cdaa55c7b5b776710b21f5aac5e561d8c2
Gottavianoni <guillaume.ottavianoni@gmail.com>
2017-04-05 16:12:09 +0200

    Update amazon.R

--- Amazon Scraping/amazon.R
+++ Amazon Scraping/amazon.R
@@ -1,5 +1,5 @@
 #MAIN
-proxy <- "http://ottavig:Pierre-Fabre03@proxy1:8080";
+proxy <- "*****";
 Sys.setenv(http_proxy = proxy);
 Sys.setenv(https_proxy = proxy);
 source("C:/users/ottavig/Documents/Script_R/Amazon Scraping/amazon_scraping.R",encoding = "UTF-8")
