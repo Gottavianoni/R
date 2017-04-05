811a1886a371364cf014fb016421cbb290c25439
Gottavianoni <guillaume.ottavianoni@gmail.com>
2017-04-05 16:23:58 +0200

    Update beaute_test_membre_batch.R

--- beaute-test/beaute_test_membre_batch.R
+++ beaute-test/beaute_test_membre_batch.R
@@ -1,9 +1,9 @@
 #TEST
-#Epurer les r�sultats
+#Epurer les résultats
 load("C:/Users/ottavig/Documents/beaute_test_membres.RData")
 library(rvest)
-Sys.setenv(http_proxy="http://ottavig:Pierre-Fabre02@proxy1:8080")
-Sys.setenv(https_proxy="http://ottavig:Pierre-Fabre02@proxy1:8080")
+Sys.setenv(http_proxy="*****")
+Sys.setenv(https_proxy="*****")
 Sys.setenv(encoding = "UTF-8")
 
 #recup des membres
@@ -29,32 +29,32 @@ pers_desc$dep <- substr(pers_desc$desc,as.numeric(gregexpr( "(",pers_desc$desc,f
 pers_desc$peau<- substr(pers_desc$caract,as.numeric(gregexpr( "Type de peau : ",pers_desc$caract,fixed = T)) + 15,as.numeric(gregexpr( "shydratation",pers_desc$caract,fixed = T)) - 4)
 pers_desc$desyd<- substr(pers_desc$caract,as.numeric(gregexpr( "hydratation : ",pers_desc$caract,fixed = T)) + 14,as.numeric(gregexpr( "Type de cheveux",pers_desc$caract,fixed = T)) - 1)
 pers_desc$cheveux <- substr(pers_desc$caract,as.numeric(gregexpr( "Type de cheveux : ",pers_desc$caract,fixed = T)) + 18,as.numeric(gregexpr( "Type de cheveux : ",pers_desc$caract,fixed = T)) + 50)
-pers_desc$dep <- gsub("é","e",pers_desc$dep)
-pers_desc$dep <- gsub("ô","o",pers_desc$dep)
-pers_desc$dep <- gsub("è","e",pers_desc$dep)
+pers_desc$dep <- gsub("Ã©","e",pers_desc$dep)
+pers_desc$dep <- gsub("Ã´","o",pers_desc$dep)
+pers_desc$dep <- gsub("Ã¨","e",pers_desc$dep)
 pers_desc$dep <- toupper(pers_desc$dep)
 pers_desc <- pers_desc[,c(1,4:9)]
 
-pers_desc$peau <- gsub("é","e",pers_desc$peau)
-pers_desc$peau <- gsub("ô","o",pers_desc$peau)
-pers_desc$peau <- gsub("è","e",pers_desc$peau)
+pers_desc$peau <- gsub("Ã©","e",pers_desc$peau)
+pers_desc$peau <- gsub("Ã´","o",pers_desc$peau)
+pers_desc$peau <- gsub("Ã¨","e",pers_desc$peau)
 
-pers_desc$cheveux <- gsub("é","e",pers_desc$cheveux)
-pers_desc$cheveux <- gsub("ô","o",pers_desc$cheveux)
-pers_desc$cheveux <- gsub("è","e",pers_desc$cheveux)
+pers_desc$cheveux <- gsub("Ã©","e",pers_desc$cheveux)
+pers_desc$cheveux <- gsub("Ã´","o",pers_desc$cheveux)
+pers_desc$cheveux <- gsub("Ã¨","e",pers_desc$cheveux)
 
-pers_desc$desyd <- gsub("é","e",pers_desc$desyd)
-pers_desc$desyd <- gsub("ô","o",pers_desc$desyd)
-pers_desc$desyd <- gsub("è","e",pers_desc$desyd)
+pers_desc$desyd <- gsub("Ã©","e",pers_desc$desyd)
+pers_desc$desyd <- gsub("Ã´","o",pers_desc$desyd)
+pers_desc$desyd <- gsub("Ã¨","e",pers_desc$desyd)
 
-pers_desc$desyd <- gsub("é","e",pers_desc$desyd)
-pers_desc$desyd <- gsub("ô","o",pers_desc$desyd)
-pers_desc$desyd <- gsub("è","e",pers_desc$desyd)
+pers_desc$desyd <- gsub("Ã©","e",pers_desc$desyd)
+pers_desc$desyd <- gsub("Ã´","o",pers_desc$desyd)
+pers_desc$desyd <- gsub("Ã¨","e",pers_desc$desyd)
 
-liste_membre <- gsub("é","e",liste_membre)
-liste_membre <- gsub("ô","o",liste_membre)
-liste_membre <- gsub("è","e",liste_membre)
-liste_membre <- gsub("�","a",liste_membre)
+liste_membre <- gsub("Ã©","e",liste_membre)
+liste_membre <- gsub("Ã´","o",liste_membre)
+liste_membre <- gsub("Ã¨","e",liste_membre)
+liste_membre <- gsub("Ã","a",liste_membre)
 
 save.image("C:/Users/ottavig/Documents/beaute_test_membres.RData")
 rm(prod,produit,prox,pseudo,pers,membre,html_personne)
